dd65f3d5f13fbdd37e6f5ef2bdf8b8245765734d
Mert <kurttutan.mert@gmail.com>
2026-03-16 09:58:58 +0000

    refactor: put layernorm module inside where it is used

--- src/rvc/synthesizer/attentions.py
+++ src/rvc/synthesizer/attentions.py
@@ -4,8 +4,22 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from rvc.synthesizer.modules import LayerNorm
 from rvc.utils import linear_channel_first
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, channels, eps=1e-5):
+        super().__init__()
+        self.channels = channels
+        self.eps = eps
+
+        self.gamma = nn.Parameter(torch.ones(channels))
+        self.beta = nn.Parameter(torch.zeros(channels))
+
+    def forward(self, x):
+        x = x.transpose(1, -1)
+        x = F.layer_norm(x, (self.channels,), self.gamma, self.beta, self.eps)
+        return x.transpose(1, -1)
 
 
 class Encoder(nn.Module):
